40150605d460e4aea07126989e0b6d1336eef5cd
Sennebs <sennebosschem@gmail.com>
2026-02-13 15:13:35 +0100

    add .gitignore

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,15 @@
 import uvicorn
+from fastapi import FastAPI
+from app.api.v1 import expenses, income
+
+app = FastAPI(
+    title="Personal Finance tracker",
+    description="API that will handle new and update existing transactions for the personal finane API.",
+    version="1.0.0"
+)
+
+app.include_router(income.router)
+app.include_router(expenses.router)
 
 if __name__ == "__main__":
     uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
